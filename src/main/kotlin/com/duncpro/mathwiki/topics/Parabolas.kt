a518c60f42c58e0592ea7f7d3b20a6c4fc5eff7e
Duncan Proctor <duncanproctor@Duncans-MacBook-Pro.local>
2023-05-22 01:08:12 -0400

    parabola rotation

--- src/main/kotlin/com/duncpro/mathwiki/topics/Parabolas.kt
+++ src/main/kotlin/com/duncpro/mathwiki/topics/Parabolas.kt
@@ -1,7 +1,38 @@
 package com.duncpro.mathwiki.topics
 
+import com.duncpro.mathwiki.graphics.Graph2d
+import com.duncpro.mathwiki.graphics.Graph2dFormat
+import com.duncpro.mathwiki.graphics.Graph2dFunction
+import com.duncpro.mathwiki.graphics.MathBlock
 import com.duncpro.mathwiki.layout.WikiSection
+import com.duncpro.webk.*
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-fun Parabolas() = WikiSection("Parabolas") {
-    +"TODO"
+fun Parabolas() = UI {
+    WikiSection("Parabolas") {
+        +WikiSection("Standard Form") {
+            +MathBlock("y=x^2")
+            +br()
+            +Graph2d(const(Graph2dFormat(
+                _fns = const(listOf(
+                    Graph2dFunction(fn = { x -> x.pow(2.0) })
+                ))
+            )))
+        }
+        +WikiSection("Rotation 90 degrees") {
+            +MathBlock("""y=\pm\sqrt{x}""")
+            +br()
+            +Graph2d(const(
+                Graph2dFormat(
+                _fns = const(listOf(
+                    Graph2dFunction(fn = { x -> sqrt(x) }),
+                    Graph2dFunction(fn = { x -> -1 * sqrt(x) })
+                )))
+            ))
+        }
+        +WikiSection("Rotation -90 degrees") {
+            +Graph2d()
+        }
+    }
 }
