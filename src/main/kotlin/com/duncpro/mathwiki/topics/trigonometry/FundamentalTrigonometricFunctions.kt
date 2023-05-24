82fa8cdf372aef7402072b4ffa0f7b6637d2096e
Duncan Proctor <duncanproctor@Duncans-MacBook-Pro.local>
2023-05-23 23:56:26 -0400

    fundamental trig functions

--- src/main/kotlin/com/duncpro/mathwiki/topics/trigonometry/FundamentalTrigonometricFunctions.kt
+++ src/main/kotlin/com/duncpro/mathwiki/topics/trigonometry/FundamentalTrigonometricFunctions.kt
@@ -19,7 +19,8 @@ fun FundamentalTrigonometricFunctions() = WikiSection("Fundamental Trigonometric
             +"On the unit circle, "; +Math("\\cos(\\theta)"); +" is equal the x-coordinate of the point intersecting ";
             +"the circle at angle "; +Math("\\theta"); +". This follows from the premise that the length of the ";
             +"hypotenuse of the right triangle of a circle is always constant with respect to the circle. Specifically, "
-            +"the length of the hypotenuse is equal to the radius of the circle."
+            +"the length of the hypotenuse is equal to the radius of the circle, and the radius of the unit circle is ";
+            +"1."
         }
         +MathBlock("\\cos(\\theta) = \\frac{\\text{adjacent}}{\\text{hypotenuse}}")
     }
@@ -32,6 +33,20 @@ fun FundamentalTrigonometricFunctions() = WikiSection("Fundamental Trigonometric
         +p {
             +"The leg parallel with the y-axis is typically called the "; +b { +"opposite" }; +" leg."
         }
+        +p {
+            +"On the unit circle, "; +Math("\\sin(\\theta)"); +" is equal to the y-coordinate of the point ";
+            +"intersecting the circle at angle "; +Math("\\theta"); +". This follows from the premise that ";
+            +"hypotenuse of the right triangle of a circle is constant with respect to the circle. The hypotenuse of ";
+            +"the unit circle has a length of 1 (equal to the radius)."
+        }
         +MathBlock("\\sin(\\theta) = \\frac{\\text{opposite}}{\\text{hypotenuse}}")
+    }
+    +WikiSection("Tangent") {
+        +p {
+            +"Given the measure of the angle at the intersection of the hypotenuse and the x-axis, the ";
+            +Math("\\tan(\\theta)"); +" function returns the ratio of the length of the leg parallel to the y-axis ";
+            +" to the length of the leg parallel with the x-axis."
+        }
+        +MathBlock("\\sin(\\theta)=\\frac{\\text{opposite}}{\\text{adjacent}}")
     }
 }
